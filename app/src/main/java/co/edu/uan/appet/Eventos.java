493c32ddeb7ea4b5745671e033255fe0d5254536
Luis Alberto <luisgutierrezr@gmail.com>
2016-10-04 19:58:42 -0500

    Entrega 2

--- app/src/main/java/co/edu/uan/appet/Eventos.java
+++ app/src/main/java/co/edu/uan/appet/Eventos.java
@@ -1,7 +1,12 @@
 package co.edu.uan.appet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Eventos extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class Eventos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventos);
+        ListView list = (ListView) findViewById(R.id.lvEventos);
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> list,
+                                            View row,
+                                            int index,
+                                            long rowID) {
+                        editarEvento(row);
+                    }
+                }
+        );
+
+    }
+
+    public void editarEvento(View v){
+        Intent intent = new Intent(this, Evento.class);
+        startActivity(intent);
     }
 }
